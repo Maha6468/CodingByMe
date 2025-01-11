4a6fddad006dca066286e193d5e2eaa586659e1a
maha <mahabubarmaha@gmail.com>
2025-01-11 13:41:41 +0600

    Beecrowed

---
+++ src/PracticesByBeeCrowed/BeeCrowed1001.java
@@ -0,0 +1,14 @@
+package PracticesByBeeCrowed;
+
+import java.util.Scanner;
+
+public class BeeCrowed1001 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner (System.in);
+        int A,B,X;
+        A=input.nextInt();
+        B=input.nextInt();
+        X=A+B;
+        System.out.println("X = "+X);
+    }
+}
