1e43043ece346c340a23e368828d6991759d50f6
maha <mahabubarmaha@gmail.com>
2025-01-11 13:37:11 +0600

    Beecrowed

---
+++ src/PracticesByBeeCrowed/BeeCrowed1003.java
@@ -0,0 +1,13 @@
+package PracticesByBeeCrowed;
+
+import java.util.Scanner;
+
+public class BeeCrowed1003 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner (System.in);
+        int A,B;
+        A=input.nextInt();
+        B=input.nextInt();
+        System.out.println("SOMA = "+(A+B));
+    }
+}
