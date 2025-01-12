2b8951bee370f340faa5fb2f5b6c4dba3a44e837
maha <mahabubarmaha@gmail.com>
2025-01-12 19:05:51 +0600

    Beecrowed

--- src/PracticesByBeeCrowed/BeeCrowed1009.java
+++ src/PracticesByBeeCrowed/BeeCrowed1009.java
@@ -1,4 +1,17 @@
 package PracticesByBeeCrowed;
 
+import java.util.Scanner;
+
 public class BeeCrowed1009 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner (System.in);
+        String name=input.nextLine();
+        double salary= input.nextDouble();
+        double sales= input.nextDouble();
+        double total;
+        total=((sales*15/100)+salary);
+        System.out.printf("TOTAL = R$ %.2f\n",total);
+
+
+    }
 }
