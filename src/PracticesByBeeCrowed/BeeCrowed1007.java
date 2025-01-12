aee98112e49dd51576e7e97eb4844f24de206b1a
maha <mahabubarmaha@gmail.com>
2025-01-12 17:43:01 +0600

    Beecrowed

---
+++ src/PracticesByBeeCrowed/BeeCrowed1007.java
@@ -0,0 +1,16 @@
+package PracticesByBeeCrowed;
+
+import java.util.Scanner;
+
+public class BeeCrowed1007 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner (System.in);
+        int A=input.nextInt();
+        int B=input.nextInt();
+        int C=input.nextInt();
+        int D=input.nextInt();
+        D=(A*B-C*D);
+        System.out.println("DIFERENCA = "+D);
+
+    }
+}
