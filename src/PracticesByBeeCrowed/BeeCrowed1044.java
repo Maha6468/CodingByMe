d45d7e6e6a0f53df6d7358257b942aae478a8619
maha <mahabubarmaha@gmail.com>
2025-01-15 00:38:10 +0600

    Beecrowed

--- src/PracticesByBeeCrowed/BeeCrowed1044.java
+++ src/PracticesByBeeCrowed/BeeCrowed1044.java
@@ -7,7 +7,7 @@ public class BeeCrowed1044 {
         Scanner input=new Scanner (System.in);
         int A= input.nextInt();
         int B= input.nextInt();
-        if((A%B==0)||(B%A==0)){  // if(A%B==0) just ata korcilam vulhoicilo ,karon bro number ta age or pore thakte pare
+        if((A%B==0)||(B%A==0)){  // if(A%B==0)  ata korcilam vulhoicilo ,karon bro number ta age or pore thakte pare
             System.out.println("Sao Multiplos");
         }else
             System.out.println("Nao sao Multiplos");
