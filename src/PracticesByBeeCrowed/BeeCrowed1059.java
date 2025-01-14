49f28d598dfb13efc5595f3b4de340bd5df7c775
maha <mahabubarmaha@gmail.com>
2025-01-15 00:52:27 +0600

    Beecrowed

---
+++ src/PracticesByBeeCrowed/BeeCrowed1059.java
@@ -0,0 +1,16 @@
+package PracticesByBeeCrowed;
+
+import java.util.Scanner;
+
+public class BeeCrowed1059 {
+    public static void main(String[] args) {
+        //Scanner input=new Scanner (System.in);
+
+        for(int i=1;i<=100;i++){
+            if(i%2==0){
+            System.out.println(i);
+            }
+        }
+
+    }
+}
