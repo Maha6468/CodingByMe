3116853ea7b380f022415aa9550d2d51daa75d0f
maha <mahabubarmaha@gmail.com>
2025-01-15 01:49:42 +0600

    Beecrowed

--- src/PracticesByBeeCrowed/BeeCrowed1060.java
+++ src/PracticesByBeeCrowed/BeeCrowed1060.java
@@ -5,14 +5,31 @@ import java.util.Scanner;
 public class BeeCrowed1060 {
     public static void main(String[] args) {
         Scanner input=new Scanner (System.in);
-        int count=0;
+       /* int count=0;
         for(int i=1;i<=6;i++){
             double number= input.nextDouble();
             if(number>0){
                 count++;
             }
         }
-        System.out.println(count+"valores positivos");
+        System.out.println(count+"valores positivos");*/
+
+
+
+                //avabeo hoito
+                int[] numbers = {-3, 5, -7, 12, 8, -1};
+
+                int positiveCount = 0;
+                for (int i = 0; i < numbers.length; i++) {
+                    if (numbers[i] > 0) {
+                        positiveCount++;
+                    }
+                }
+
+
+                System.out.println("Number of positive numbers: " + positiveCount);
+
+
 
 
 
