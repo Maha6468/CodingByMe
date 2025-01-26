6ab7c29a6a168658da7586e243d89ef1ec9e2f50
Maha6468 <motherma7633@gmail.com>
2025-01-26 20:25:04 +0600

    compare

--- src/Practice/CompareArrays.java
+++ src/Practice/CompareArrays.java
@@ -6,12 +6,30 @@ import java.util.Scanner;
 public class CompareArrays {
     public static void main(String[] args) {
         Scanner input=new Scanner (System.in);
-        int[] arr1={15,12,5,20,35};
+       /* int[] arr1={15,12,5,20,35};
         int[] arr2={15,12,20,35};
         if(Arrays.equals(arr1,arr2)){
             System.out.println("Both are equal.");
         }else
+            System.out.println("Both are not equal.");*/
+
+        int[] arr1=new int[4];
+        int[] arr2=new int[4];
+        System.out.print("First array: ");
+        for(int i=0;i<4;i++){
+            arr1[i]=input.nextInt();
+        }
+        System.out.print("Second array: ");
+        for(int i=0;i<4;i++){
+            arr2[i]= input.nextInt();
+        }
+        if(Arrays.equals(arr1,arr2)){
+            System.out.println("Both are equal.");
+        }else
             System.out.println("Both are not equal.");
+
+
+
 
     }
 }
