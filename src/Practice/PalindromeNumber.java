66caa40810963a20b993782ab347232d8f81d4b2
Maha6468 <motherma7633@gmail.com>
2025-02-05 01:16:16 +0600

    .

--- src/Practice/PalindromeNumber.java
+++ src/Practice/PalindromeNumber.java
@@ -9,7 +9,7 @@ public class PalindromeNumber {
         int revers=0,remind;
         int i=0;
         int store=n;
-        for(;0<n;i++){              //for(int i=0;0<n;i++)
+        for(;0<n;i++){              //for(int i=0;0<n;i++) ata korleo hoy
              remind=n%10;
             revers=revers*10+remind;
             n/=10;
