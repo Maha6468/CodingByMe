7049bf4976aba3324c13385e709b1fb13cd60beb
Maha6468 <motherma7633@gmail.com>
2025-02-05 01:14:33 +0600

    .

---
+++ src/Practice/PalindromeNumber.java
@@ -0,0 +1,26 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        Scanner input=new Scanner (System.in);
+        int n= input.nextInt();
+        int revers=0,remind;
+        int i=0;
+        int store=n;
+        for(;0<n;i++){              //for(int i=0;0<n;i++)
+             remind=n%10;
+            revers=revers*10+remind;
+            n/=10;
+        }
+        System.out.println(revers);
+        if(store==n){
+            System.out.println("It's palindrom number");
+        }else {
+            System.out.println("It's not a palindrom number");
+        }
+
+
+    }
+}
